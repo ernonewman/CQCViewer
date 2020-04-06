59f65a46e91fd04d284d376a99dba08b5401b431
Erno Newman <ernonewman@gmail.com>
2020-04-06 19:13:01 +0100

    Added Services and their Interfaces

--- CQCViewer.Shared/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CQCViewer.Shared
-{
-    public class Class1
-    {
-    }
-}
---
+++ CQCViewer.Shared/Services/ProvidersSummaryServices.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using CQCViewer.Shared.HttpClients;
+using CQCViewer.Shared.Interfaces.Services;
+using CQCViewer.Shared.Models;
+
+namespace CQCViewer.Shared.Services
+{
+    public class ProviderSummaryServices : IProvidersSummaryServices
+    {
+        private readonly ProviderSummaryHttpClient _client;
+        public ProviderSummaryServices(ProviderSummaryHttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<ProvidersSummary> GetProviderSummary()
+        {
+            return await _client.GetContentFromClient().ConfigureAwait(false);
+        }
+    }
+}
