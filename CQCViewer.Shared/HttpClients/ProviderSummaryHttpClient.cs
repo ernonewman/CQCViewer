ea6c577f16d4f31f5f227c62cb27acda51b1d826
Erno Newman <ernonewman@gmail.com>
2020-04-06 16:38:55 +0100

    Added HttpClient for ProviderSummary

---
+++ CQCViewer.Shared/HttpClients/ProviderSummaryHttpClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using CQCViewer.Shared.Models;
+
+namespace CQCViewer.Shared.HttpClients
+{
+    public class ProviderSummaryHttpClient
+    {
+        private HttpClient _client;
+
+        public ProviderSummaryHttpClient(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<ProviderSummary> GetContentFromClient()
+        {
+            try
+            {
+                var options = new JsonSerializerOptions
+                {
+                    AllowTrailingCommas = true
+                };
+
+
+                var json = await _client.GetStreamAsync("https://api.cqc.org.uk/public/v1/providers").ConfigureAwait(false);
+
+                return await JsonSerializer.DeserializeAsync<ProviderSummary>(json, options);
+            }
+            catch (HttpRequestException ex)
+            {
+                throw;
+            }
+        }
+    }
+}
